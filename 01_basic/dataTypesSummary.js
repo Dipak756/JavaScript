020258cfe1bf62a51247b58c3965ae790730800d
Dipak756 <117346877+Dipak756@users.noreply.github.com>
2024-02-23 06:49:21 +0000

    Data Type Summary

---
+++ 01_basic/dataTypesSummary.js
@@ -0,0 +1,16 @@
+// There are two type of data  type primitive and non-primitive(reference)
+
+// Primitive are divided into 7 parts, "string", Number, Boolean, Undefined, Null, Symbol, and BigInt
+ // typeof String is string
+ // typeof Number is number
+ // typeof Boolean is Boolean
+ // typeof Undefined id undefined
+//  typeof Null is object
+// typeof Symbol is symbol
+// typeof BigInt is Boolean
+
+
+//Reference or non-prmitive are Arrays, Objects and Functions
+
+//typeof Array and Objects are function
+// typeof Functions are function objects
